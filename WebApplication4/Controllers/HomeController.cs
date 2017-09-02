5b36dd32113818813b9af073d2a60d541b6d6784
mac <mac@DESKTOP-KO7KV5Q>
2017-09-02 19:34:50 +0200

    ReactNet tutorial

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Lojza" + DateTime.Now.ToString(),
+                    Text = "Alois"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pepa",
+                    Text = "JoseF"
+                }
+            };
+        }
+
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+        [Route("comments/new")]
+        
+        public ActionResult AddComment(CommentModel comment)
+        {
+            comment.Id = _comments.Count() + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+    }
+}
---
+++ WebApplication4/wwwroot/js/tutorial.jsx
@@ -0,0 +1,134 @@
+﻿
+var data = [
+    { id: 1, author: "Karel Čapek", text: "Válka" },
+    { id: 2, author: "Jarda Novák", text: "Pohádka" },
+    { id: 3, author: "Cyril Nobl", text: "Studna" }
+];
+
+var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }, 
+
+    render: function () {
+        
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function () {
+        var commentNodes = this.props.data.map(function (comment){
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                    </Comment>
+                );
+        });
+
+        return (
+            <div className="commentList">
+               {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    getInitialState: function () {
+        return { author: '', text: '' };
+    },
+    handleAuthorChange: function (e) {
+        this.setState({ author: e.target.value });
+    },
+    handleTextChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+    handleSubmit: function (e) {
+
+        alert('js kotel');
+    
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        //toto: send to server
+        this.props.onCommentSubmit({author: author, text: text });
+        th.setState({author: '', text: ''});
+    },
+    render: function () {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Say something ..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+
+    getInitialState: function () {
+        return { data: [] };
+    },
+    loadCommentsFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    componentDidMount: function () {
+        this.loadCommentsFromServer();
+        window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+    handleCommentSubmit: function (comment) {
+        //todo submit to server and refresh list
+        var data = new FormData();
+        data.append('author', comment.author);
+        data.append('text', comment.text);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', this.props.submitUrl, true);
+        xhr.onload = function () {
+            this.loadCommentsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    },
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+            </div>
+            );
+    }
+});
+
+ReactDOM.render(<CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
+    document.getElementById('content'));
